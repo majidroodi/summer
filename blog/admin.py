4ee0e77573f41b1d703a601d5a48b8ae96e0d165
Peyman Salehi <s.peyman94@gmail.com>
2017-08-29 01:05:10 +0430

    feat(blog, contact): add filtering and change list display add filter, search and edit list display in contact and blog admin page

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models import Post, PostCategory
 
 class PostAdmin(admin.ModelAdmin):
-    list_filter = ['pub_date']
+    list_display = ['name', 'post_category', 'pub_date', 'last_modified', 'author']
+    list_filter = ['pub_date', 'author', 'post_category']
+    prepopulated_fields = { 'slug': ('name',) }
+    search_fields = ['name', 'content']
+
+class PostCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = { 'slug': ('name',) }
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(PostCategory)
+admin.site.register(PostCategory, PostCategoryAdmin)
--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Letter, LetterSubject
 
-admin.site.register(Letter)
-admin.site.register(LetterSubject)
+class LetterAdmin(admin.ModelAdmin):
+    list_display = ['client_name', 'client_email', 'letter_subject', 'postage_date']
+    list_filter = ['postage_date', 'letter_subject']
+
+
+class LetterSubjectAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_editable = ['title']
+    list_display_links = None
+
+
+admin.site.register(Letter, LetterAdmin)
+admin.site.register(LetterSubject, LetterSubjectAdmin)
