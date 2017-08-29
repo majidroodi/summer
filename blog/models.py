8fd8aaff4d22e86d9120e21bd0483cc9718f5168
Peyman Salehi <s.peyman94@gmail.com>
2017-08-29 23:56:42 +0430

    feat(blog): add jalali date and some field in blog model

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
-
+from django_jalali.db import models as jmodels
 
 
 class PostCategory(models.Model):
@@ -20,14 +21,17 @@ class PostCategory(models.Model):
 class Post(models.Model):
     name = models.CharField(max_length=200, verbose_name=_('Name'))
     slug = models.SlugField(max_length=200, verbose_name=_('Slug'), help_text=_('Used in the URL'))
-    content = RichTextUploadingField(verbose_name=_('Content'))
+    brief_content = models.TextField(verbose_name=_('Brief content'), null=True)
+    content = RichTextUploadingField(verbose_name=_('Content'), null=True)
     post_category = models.ForeignKey(PostCategory, on_delete=models.CASCADE, verbose_name=_('Category'))
-    pub_date = models.DateField(auto_now_add=True, verbose_name=_('Publish date'))
-    last_modified = models.DateField(auto_now=True, verbose_name=_('Modified date'))
+    pub_date = jmodels.jDateField(verbose_name=_('Publish date'))
+    last_modified = jmodels.jDateField(auto_now=True, verbose_name=_('Modified date'))
+    pub_status = models.BooleanField(default=True, verbose_name=_('Publish status'))
     author = models.ForeignKey(User, null=True, blank=True, verbose_name=_('Author'))
     thumbnail = models.ImageField(
         upload_to='thumbnails',
-        verbose_name=_('Thumbnail')
+        verbose_name=_('Thumbnail'),
+        null=True
     )
 
     def __str__(self):
--- summer/settings.py
+++ summer/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jalali',
     'blog.apps.BlogConfig',
     'contact.apps.ContactConfig',
     'ckeditor',
